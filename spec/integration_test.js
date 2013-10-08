bb65e3a15df436bae529e3e071da305a6fba622d
sunwenpeng <1014188472@qq.com>
2013-10-08 17:48:40 +0800

    集成测试—点击‘产生一个随机数’按钮,输入6次随机数分别点击‘确认’按钮输出您已猜了6次！

--- spec/integration_test.js
+++ spec/integration_test.js
@@ -7,5 +7,26 @@ describe('integration test',function(){
         var result=document.getElementById("txt2").innerText == "2A1B"    ;
         expect(result).toBeTruthy();
     });
+    it('result should be 您已猜了6次！',function(){
+        times=0;
+        btn_click()  ;
+        localStorage.setItem("random_number", 1234);
+        document.getElementById('guess').value = 1435 ;
+        btn_click_1();
+        document.getElementById('guess').value = 1425 ;
+        btn_click_1();
+        document.getElementById('guess').value = 1436 ;
+        btn_click_1();
+        console.log(times);
+        document.getElementById('guess').value = 9435 ;
+        btn_click_1();
+        document.getElementById('guess').value = 1437 ;
+        btn_click_1();
+        document.getElementById('guess').value = 2435 ;
+        btn_click_1();
+        console.log(times);
+        var result=document.getElementById("txt2").innerText == "您已猜了6次！";
+        expect(result).toBeTruthy();
+    });
 
 })
