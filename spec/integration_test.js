dfd760e6f5e720f4a6fe218af7a590d50493d7a3
sunwenpeng <1014188472@qq.com>
2013-10-08 17:50:31 +0800

    集成测试—点击‘产生一个随机数’按钮,第二次输入正确随机数点击‘确认’按钮输出您已猜对了！

--- spec/integration_test.js
+++ spec/integration_test.js
@@ -28,5 +28,15 @@ describe('integration test',function(){
         var result=document.getElementById("txt2").innerText == "您已猜了6次！";
         expect(result).toBeTruthy();
     });
-
+    it('result should be 猜对了！',function(){
+        times=0   ;
+        btn_click()  ;
+        localStorage.setItem("random_number", 1234);
+        document.getElementById('guess').value = 1435 ;
+        btn_click_1();
+        document.getElementById('guess').value = 1234 ;
+        btn_click_1();
+        var result=document.getElementById("txt2").innerText == "猜对了！";
+        expect(result).toBeTruthy();
+    });
 })
