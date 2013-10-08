7e706727aea1a3a13ab3daf21f8096df621d11ea
sunwenpeng <1014188472@qq.com>
2013-10-08 17:39:58 +0800

    点击‘确定’输出您已猜了6次

--- spec/button_test.js
+++ spec/button_test.js
@@ -13,6 +13,27 @@ describe('button_test',function(){
         var result=document.getElementById("txt2").innerText == "2A1B"    ;
         expect(result).toBeTruthy();
     });
+    it('onclick 确认 should put out 您已猜了6次！',function(){
+        times=0;
+        btn_click()  ;
+        localStorage.setItem("random_number", 1234);
+        document.getElementById('guess').value = 1435 ;
+        btn_click_1();
+        document.getElementById('guess').value = 1425 ;
+        btn_click_1();
+        document.getElementById('guess').value = 1436 ;
+        btn_click_1();
+        console.log(times);
+        document.getElementById('guess').value = 9435 ;
+        btn_click_1();
+        document.getElementById('guess').value = 1437 ;
+        btn_click_1();
+        document.getElementById('guess').value = 2435 ;
+        btn_click_1();
+        console.log(times);
+        var result=document.getElementById("txt2").innerText == "您已猜了6次！";
+        expect(result).toBeTruthy();
+    })
 
 
 })
