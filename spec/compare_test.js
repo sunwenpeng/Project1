f863a532e8de8706d210e56791945d4d0cd3c3b8
sunwenpeng <1014188472@qq.com>
2013-10-08 17:30:16 +0800

    比较两个数是0A0B时返回值是00

---
+++ spec/compare_test.js
@@ -0,0 +1,45 @@
+describe('comparetest',function(){
+    it('result should be 0,0',function(){
+        var result;
+        result=  compare(1234,5678)[0]==0&&compare(1234,5678)[1]==0   ;
+        expect(result).toBeTruthy()
+    });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**/
